8f114158446f446aaaf4def455a57271183e83a2
Max Klenk <max@klenk.biz>
2016-11-01 13:35:07 +0100

    run type command in same folder on win

--- task1.3/runWin.sh
+++ task1.3/runWin.sh
@@ -5,6 +5,8 @@ cd _build
 cmake ..
 cmake --build .
 
-type "..\addressdata.txt" | Debug/validate_emails.exe
-type "..\addressdata.txt" | Debug/validate_emails.exe --print-valid
-type "..\addressdata.txt" | Debug/validate_emails.exe --print-invalid
+cd ..
+type addressdata.txt | _build/Debug/validate_emails.exe
+type addressdata.txt | _build/Debug/validate_emails.exe --print-valid
+type addressdata.txt | _build/Debug/validate_emails.exe --print-invalid
+cd _build
