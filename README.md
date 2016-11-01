80bcfd76ebc8a5a637905548f0d334cfe3d1c801
Max Klenk <max@klenk.biz>
2016-11-01 16:10:49 +0100

    clean project and add readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# run-cpp [![Build Status](https://travis-ci.org/maxklenk/run-cpp.svg?branch=master)](https://travis-ci.org/maxklenk/run-cpp) [![Build status](https://ci.appveyor.com/api/projects/status/wf1w0b6ruv27gp5e?svg=true)](https://ci.appveyor.com/project/maxklenk/run-cpp)
+# Advanced Programming in C++ - Assignment 1
+
+## How to run
+
+Execute `runAll.sh` (or `runAllWin.sh` on a Windows machine) to build and run all tasks using cmake.
+
--- task1.3/runWin.sh
+++ task1.3/runWin.sh
@@ -5,8 +5,6 @@ cd _build
 cmake ..
 cmake --build .
 
-cd ..
-type addressdata.txt | _build/Debug/validate_emails.exe
-type addressdata.txt | _build/Debug/validate_emails.exe --print-valid
-type addressdata.txt | _build/Debug/validate_emails.exe --print-invalid
-cd _build
+type "..\addressdata.txt" | _build/Debug/validate_emails.exe
+type "..\addressdata.txt" | _build/Debug/validate_emails.exe --print-valid
+type "..\addressdata.txt" | _build/Debug/validate_emails.exe --print-invalid
