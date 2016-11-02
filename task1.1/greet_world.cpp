66b67dc706996d9d78f78378a068db99527a954d
Max Klenk <max@klenk.biz>
2016-11-02 10:46:53 +0100

    update task1.1 based on lecture

--- task1.1/greet_world.cpp
+++ task1.1/greet_world.cpp
@@ -10,29 +10,29 @@ int main(int argc, char *argv[]) {
     std::cout << "Hello ";
 
     if (argc == 1) {
-        std::cout << "World";
+        std::cout << "World" << std::endl;
+        return 0;
     }
-    else if (argc == 2) {
-        std::cout << argv[1];
+
+    if (argc == 2) {
+        std::cout << argv[1] << std::endl;
+        return 0;
     }
-    else if (argc == 3) {
-        std::cout << argv[1] << " and " << argv[2];
+
+    if (argc == 3) {
+        std::cout << argv[1] << " and " << argv[2] << std::endl;
+        return 0;
     }
-    else if (argc > 3) {
-        for (int i = 1; i < argc; i++) {
-            if (i != 1) {
-                if (argc - 1 == i) {
-                    std::cout << ", and ";
-                }
-                else {
-                    std::cout << ", ";
-                }
-            }
-            std::cout << argv[i];
+
+    std::cout << argv[1];
+    for (auto i = 2; i < argc; i++) {
+        if (i == argc - 1) {
+            std::cout << ", and ";
+        } else {
+            std::cout << ", ";
         }
+        std::cout << argv[i];
     }
-
     std::cout << std::endl;
-
     return 0;
 }
