5d4ca271861c12c854f735f4538ab906f17178a3
Max Klenk <max@klenk.biz>
2016-11-09 11:26:47 +0100

    update task2.1 to be in the range for 10000 iterations

--- task2.1/montecarlopi.cpp
+++ task2.1/montecarlopi.cpp
@@ -2,16 +2,20 @@
 #include <iostream>
 #include <random>
 #include <math.h>
+#include <typeinfo>
 
 typedef double num_type;
+//typedef long double num_type;
+//typedef float num_type;
 
 int main(int argc, char *argv[]) {
 
     std::default_random_engine generator;
     std::uniform_real_distribution <num_type> distribution(0.0, 1.0);
 
-    long inside = 0, outside = 0;
-    double ratio;
+    long inside = 0;
+    long outside = 0;
+    num_type ratio;
 
     struct Point2d {
 
@@ -25,12 +29,10 @@ int main(int argc, char *argv[]) {
     };
 
     int steps = 0;
-    while (steps != 10000) {
+    int inRange = 0;
+    while (inRange != 10000) {
 
-        Point2d point {distribution(generator), distribution(generator)};
-
-        std::cout << "point is x: " << point.x << " y:" << point.y << std::endl;
-        std::cout << "dist is " << point.dist() << " less 1? " << (point.dist() < 1.0 ? " true" : " false") << std::endl;
+        Point2d point{distribution(generator), distribution(generator)};
 
         if (point.dist() < 1.0) {
             inside++;
@@ -38,16 +40,21 @@ int main(int argc, char *argv[]) {
             outside++;
         }
 
-        ratio =  4.0 * double(inside) / ( double(inside) + double(outside) );
-        std::cout << "inside: " << inside << " outside: " << outside << " ratio is " << ratio << std::endl;
+        ratio = num_type(4) * inside / (inside + outside);
 
-        if (ratio > 3.1413 && ratio < 3.1419) {
-            std::cout << "PI sucks! ";
-            break;
+        if (ratio > num_type(3.1413) && ratio < num_type(3.1419)) {
+            inRange++;
+        } else {
+            inRange = 0;
         }
+
         steps++;
+
     }
+
+    std::cout.precision(128);
     std::cout << "steps: " << steps << std::endl;
+    std::cout << "PI: " << ratio << " " << typeid(ratio).name() << " " << sizeof(ratio) << std::endl;
 }
 
 
