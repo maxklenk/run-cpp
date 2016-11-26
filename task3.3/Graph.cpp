3d43fd8438e82c30ce61b0ff761aa6eca3c4b081
Max Klenk <max@klenk.biz>
2016-11-26 18:36:52 +0100

    add bonus task MST using prim

--- task3.3/Graph.cpp
+++ task3.3/Graph.cpp
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <memory>
 
-
 //
 // Class Vertex
 //
@@ -148,16 +147,110 @@ void Graph::merge(std::shared_ptr<Graph> g2)
     g2.reset();
 }
 
+// A utility function to find the vertex with minimum key value, from
+// the set of vertices not yet included in MST
+int minKey(int key[], bool mstSet[], int amountNodes)
+{
+    // Initialize min value
+    int min = 1000000;
+    int min_index = -1;
+
+    for (int v = 0; v < amountNodes; v++) {
+        if (!mstSet[v] && key[v] < min) {
+            min = key[v], min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void solveMST(int **graph, int parent[], int amountNodes)
+{
+    //int parent[amountNodes]; // Array to store constructed MST
+    int key[amountNodes];   // Key values used to pick minimum weight edge in cut
+    bool mstSet[amountNodes];  // To represent set of vertices not yet included in MST
+
+    // Initialize all keys as INFINITE
+    for (int i = 0; i < amountNodes; i++) {
+        key[i] = 1000000;
+        mstSet[i] = false;
+    }
+
+
+    // Always include first 1st vertex in MST.
+    key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
+    parent[0] = -1; // First node is always root of MST
+
+    // The MST will have V vertices
+    for (int count = 0; count < amountNodes - 1; count++) {
+        // Pick thd minimum key vertex from the set of vertices
+        // not yet included in MST
+        int u = minKey(key, mstSet, amountNodes);
+
+        // Add the picked vertex to the MST Set
+        mstSet[u] = true;
+
+        // Update key value and parent index of the adjacent vertices of
+        // the picked vertex. Consider only those vertices which are not yet
+        // included in MST
+        for (int v = 0; v < amountNodes; v++) {
+
+            // graph[u][v] is non zero only for adjacent vertices of m
+            // mstSet[v] is false for vertices not yet included in MST
+            // Update the key only if graph[u][v] is smaller than key[v]
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u, key[v] = graph[u][v];
+            }
+        }
+    }
+}
 
 std::shared_ptr<Graph> Graph::minimumSpanningTree() const
 {
-    std::unique_ptr<Graph> graph =std::make_unique<Graph>();
+    // create array representation of graph for faster lookups
+    int amountNodes = m_vertices.size();
+    int **graph = new int*[amountNodes];
 
-    //
-    // TODO: implement minimum spanning tree computation
-    //
+    for (int i = 0; i < amountNodes; i++) {
+        graph[i] = new int[amountNodes];
+        for (int j = 0; j < amountNodes; j++) {
+            graph[i][j] = 0;
+        }
+    }
 
-    return std::shared_ptr<Graph>(std::move(graph));
+    int amountEdges = m_edges.size();
+    for (const auto &edge : m_edges) {
+        auto from = edge->v0()->id();
+        auto to = edge->v1()->id();
+        auto weight = edge->weight();
+        graph[from][to] = weight;
+        graph[to][from] = weight;
+    }
+
+
+    // solve MST (prim)
+    int parent[amountNodes];
+    solveMST(graph, parent, amountNodes);
+
+
+    // construct new graph
+    auto resultGraph = std::make_unique<Graph>();
+
+    // add nodes
+    for (const auto &vertex : m_vertices) {
+        resultGraph->addVertex(vertex);
+    }
+
+    // add edges
+    for (const auto &edge : m_edges) {
+        auto from = edge->v0()->id();
+        auto to = edge->v1()->id();
+        if (parent[from] == to || parent[to] == from) {
+            resultGraph->addEdge(edge);
+        }
+    }
+
+    return std::shared_ptr<Graph>(std::move(resultGraph));
 }
 
 
