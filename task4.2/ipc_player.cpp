0674b136314777de6cc0b6da388d32ec2df6bb78
Max Klenk <max@klenk.biz>
2016-12-08 15:30:16 +0100

    simple read and write ipc

--- task4.2/ipc_player.cpp
+++ task4.2/ipc_player.cpp
@@ -2,21 +2,42 @@
 #include <iostream>
 
 #include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 
 #include "ipc_player.h"
 
 using namespace boost::interprocess;
 
-std::uint32_t readScore(std::uint32_t playerID)
-{
+std::uint32_t readScore(std::uint32_t playerID) {
     std::cout << playerID << ": read " << std::endl;
-	return 0;
+
+    shared_memory_object shm{open_or_create, "HighScore", read_write};
+    shm.truncate(1024);
+
+    // Map the whole shared memory in this process
+    mapped_region region(shm, read_only);
+
+    // Check that memory
+    std::uint32_t *i1 = static_cast<std::uint32_t *>(region.get_address());
+    i1++;
+
+    return *i1;
 }
 
 
-void updateScore(std::uint32_t playerID, std::uint32_t newScore)
-{
-	std::cout << playerID << ": update " << newScore << std::endl;
+void updateScore(std::uint32_t playerID, std::uint32_t newScore) {
+    std::cout << playerID << ": update " << newScore << std::endl;
+
+    shared_memory_object shm{open_or_create, "HighScore", read_write};
+    shm.truncate(1024);
+
+    // Map the whole shared memory in this process
+    mapped_region region(shm, read_write);
+
+    int *i1 = static_cast<int *>(region.get_address());
+    *i1 = playerID;
+    i1++;
+    *i1 = newScore;
 
     return;
 }
--- task4.2/ipc_viewer.cpp
+++ task4.2/ipc_viewer.cpp
@@ -2,14 +2,31 @@
 #include <iostream>
 
 #include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 
 #include "ipc_viewer.h"
 
 using namespace boost::interprocess;
 
-std::map<std::uint32_t, std::uint32_t> readHighScore(bool block)
-{
-	std::cout << "read " << block << std::endl;
+std::map <std::uint32_t, std::uint32_t> readHighScore(bool block) {
+    std::cout << "read " << block << std::endl;
 
-	return {};
+    auto myMap = std::map<std::uint32_t, std::uint32_t>();
+
+    shared_memory_object shm{open_or_create, "HighScore", read_write};
+    shm.truncate(1024);
+
+    // Map the whole shared memory in this process
+    mapped_region region(shm, read_only);
+
+    // Check that memory
+    int *i2 = static_cast<int *>(region.get_address());
+    std::uint32_t playerId = *i2;
+    i2++;
+    std::uint32_t score = *i2;
+
+    myMap.insert(std::make_pair(playerId, score));
+
+    return myMap;
 }
+
