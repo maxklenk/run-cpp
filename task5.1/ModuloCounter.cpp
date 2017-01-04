fb05e06dedac8805d93e4ab09bdeff82feb340c5
Max Klenk <max@klenk.biz>
2017-01-04 11:45:34 +0100

    5.1 add getter/setters and clean code

--- task5.1/ModuloCounter.cpp
+++ task5.1/ModuloCounter.cpp
@@ -1,1 +1,50 @@
-// TODO:: move implementation of class here
+#include <iostream>
+
+#import "ModuloCounter.h"
+
+void ModuloCounter::increment(int i) {
+    this->setValue(this->value + i);
+}
+
+void ModuloCounter::decrement(int i) {
+    this->setValue(this->value - i);
+}
+
+const int ModuloCounter::getValue() {
+    return this->value;
+}
+
+void ModuloCounter::setValue(int i) {
+    while (i >= this->max) {
+        i = i - (this->max - this->min);
+    }
+    while (i < this->min) {
+        i = i + (this->max - this->min);
+    }
+    this->value = i;
+}
+
+void ModuloCounter::setValue(ModuloCounter m) {
+    this->setValue(m.getValue());
+}
+
+const int ModuloCounter::getMin() {
+    return this->min;
+}
+
+void ModuloCounter::setMin(int i) {
+    this->min = i;
+}
+
+const int ModuloCounter::getMax() {
+    return this->max;
+}
+
+void ModuloCounter::setMax(int i) {
+    this->max = i;
+}
+
+std::ostream &operator<<(std::ostream &os, const ModuloCounter &counter) {
+    os << counter.value << " in [" << counter.min << ", " << counter.max << ")" << std::endl;
+    return os;
+}
--- task5.1/test.cpp
+++ task5.1/test.cpp
@@ -1,13 +1,11 @@
-
 #include <iostream>
 
 // include appropriate header file
 #include "ModuloCounter.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
     // create ModuloCounter m1 with the interval [0, 10)
-    ModuloCounter m1 {0, 10};
+    ModuloCounter m1{0, 10};
     // print m1
     std::cout << m1;
 
@@ -18,14 +16,14 @@ int main(int argc, char * argv[])
 
     // increment m1 and create ModuloCounter m2 with the new value
     m1.increment();
-    ModuloCounter m2 {0, 10, m1.getValue()};
+    ModuloCounter m2{0, 10, m1.getValue()};
     // print m1
     std::cout << m1;
     // print m2
     std::cout << m2;
 
     // decrement m2 and create ModuloCounter m3 with the old value
-    ModuloCounter m3 {0, 10, m2.getValue()};
+    ModuloCounter m3{0, 10, m2.getValue()};
     m2.decrement();
     // print m2
     std::cout << m2;
@@ -33,7 +31,7 @@ int main(int argc, char * argv[])
     std::cout << m3;
 
     // create ModuloCounter m4 with the interval [-7, 18)
-    ModuloCounter m4 {-7, 18};
+    ModuloCounter m4{-7, 18};
     // print m4
     std::cout << m4;
 
@@ -43,8 +41,7 @@ int main(int argc, char * argv[])
     std::cout << m4;
 
     // create ModuloCounter m5 with the result of multiplying m4 and m3
-    // TODO: does not create the expected output of -4
-    ModuloCounter m5 {-7, 18, m4.getValue() * m3.getValue()};
+    ModuloCounter m5{-7, 18, m4.getValue() * m3.getValue()};
     // print m5
     std::cout << m5;
 
@@ -54,7 +51,7 @@ int main(int argc, char * argv[])
     std::cout << m2;
 
     // create ModuloCounter m6 with the interval [-11, -3) and initial value of m5
-    ModuloCounter m6 {-11, -3, m5.getValue()};
+    ModuloCounter m6{-11, -3, m5.getValue()};
     // print m6
     std::cout << m6;
 
@@ -64,7 +61,7 @@ int main(int argc, char * argv[])
     std::cout << i1 << std::endl;
 
     // create ModuloCounter m7 with the result of subtracting 7 from m6
-    ModuloCounter m7 {-11, -3, m6.getValue() - 7};
+    ModuloCounter m7{-11, -3, m6.getValue() - 7};
     // print m7
     std::cout << m7;
 
