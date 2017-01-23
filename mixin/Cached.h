2cb4dd644ec68eb6f05eb116eadd60d61789f4d2
Max Klenk <max@klenk.biz>
2017-01-23 17:31:31 +0100

    add some interpolation

--- mixin/Cached.h
+++ mixin/Cached.h
@@ -4,18 +4,20 @@
 #include <utility>
 #include <vector>
 
-template <class Base>
-class Cached: public Base
-{
+template<class Base>
+class Cached : public Base {
 public:
-    explicit Cached(const std::pair<float, float> & center, float radius) : Base(center, radius) {
+    explicit Cached(const std::pair<float, float> &center, float radius) : Base(center, radius) {
         this->resulution = 0;
         this->interpolation = false;
     }
-    explicit Cached(const std::pair<float, float> & a, const std::pair<float, float> & b, const std::pair<float, float> & c, const std::pair<float, float> & d) : Base(a, b, c, d) {
+
+    explicit Cached(const std::pair<float, float> &a, const std::pair<float, float> &b,
+                    const std::pair<float, float> &c, const std::pair<float, float> &d) : Base(a, b, c, d) {
         this->resulution = 0;
         this->interpolation = false;
     };
+
     virtual ~Cached() = default;
 
     virtual void setCacheResolution(int res) {
@@ -25,12 +27,13 @@ public:
         this->cache = std::vector<std::pair<float, float>>(res);
 
         // fill cache
-        float step = 1.0f/res;
+        float step = 1.0f / res;
         for (int i = 0; i < res; i++) {
             this->cache[i] = Base::operator()(i * step);
         }
 
     }
+
     void setUseLinearInterpolation(bool inter) {
         this->interpolation = inter;
     }
@@ -38,9 +41,19 @@ public:
     virtual std::pair<float, float> operator()(float t) const {
         float step = 1.0f / this->resulution;
         if (this->interpolation) {
-            // TODO: add
-            int index = 0;
-            return this->cache[index];
+            float pos = t / step;
+            int indexS = int(pos);
+            int indexB = indexS + 1;
+
+            double interpolation = 0.5; //pos - indexS;
+
+            return std::pair<float, float>(
+                    this->cache[indexS].first +
+                    interpolation * (this->cache[indexB].first - this->cache[indexS].first),
+                    this->cache[indexS].second +
+                    interpolation * (this->cache[indexB].second - this->cache[indexS].second)
+            );
+
         } else {
             int index = int(t / step + 0.5);
             return this->cache[index];
