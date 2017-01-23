fd0ff884215c7d5d19b2b6ec26d4ba2c1f78cd92
Max Klenk <max@klenk.biz>
2017-01-23 16:50:08 +0100

    add caching

--- mixin/Cached.h
+++ mixin/Cached.h
@@ -2,27 +2,53 @@
 #pragma once
 
 #include <utility>
+#include <vector>
 
 template <class Base>
 class Cached: public Base
 {
 public:
-    explicit Cached(const std::pair<float, float> & center, float radius) : Base(center, radius)  {};
-    explicit Cached(const std::pair<float, float> & a, const std::pair<float, float> & b, const std::pair<float, float> & c, const std::pair<float, float> & d) : Base(a, b, c, d) {};
+    explicit Cached(const std::pair<float, float> & center, float radius) : Base(center, radius) {
+        this->resulution = 0;
+        this->interpolation = false;
+    }
+    explicit Cached(const std::pair<float, float> & a, const std::pair<float, float> & b, const std::pair<float, float> & c, const std::pair<float, float> & d) : Base(a, b, c, d) {
+        this->resulution = 0;
+        this->interpolation = false;
+    };
     virtual ~Cached() = default;
 
     virtual void setCacheResolution(int res) {
         this->resulution = res;
+
+        // set cache size
+        this->cache = std::vector<std::pair<float, float>>(res);
+
+        // fill cache
+        float step = 1.0f/res;
+        for (int i = 0; i < res; i++) {
+            this->cache[i] = Base::operator()(i * step);
+        }
+
     }
     void setUseLinearInterpolation(bool inter) {
         this->interpolation = inter;
     }
 
     virtual std::pair<float, float> operator()(float t) const {
-        return Base::operator()(t);
+        float step = 1.0f / this->resulution;
+        if (this->interpolation) {
+            // TODO: add
+            int index = 0;
+            return this->cache[index];
+        } else {
+            int index = int(t / step + 0.5);
+            return this->cache[index];
+        }
     };
 
 private:
     int resulution;
     bool interpolation;
+    std::vector<std::pair<float, float>> cache;
 };
