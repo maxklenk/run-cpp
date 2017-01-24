d704c87cc52985f39b6d01555e38865c6e307f8d
Tobias Wollowski <tobias@jbbr.net>
2017-01-24 16:20:11 +0100

    corrected interpolation and separated interface of Cached from implementation

--- mixin/Cached.h
+++ mixin/Cached.h
@@ -5,63 +5,87 @@
 #include <vector>
 
 template<class Base>
-class Cached : public Base {
+class Cached : public Base
+{
 public:
-    explicit Cached(const std::pair<float, float> &center, float radius) : Base(center, radius) {
-        this->resulution = 0;
-        this->interpolation = false;
-    }
 
-    explicit Cached(const std::pair<float, float> &a, const std::pair<float, float> &b,
-                    const std::pair<float, float> &c, const std::pair<float, float> &d) : Base(a, b, c, d) {
-        this->resulution = 0;
-        this->interpolation = false;
-    };
+    using Base::Base;
 
     virtual ~Cached() = default;
 
-    virtual void setCacheResolution(int res) {
-        this->resulution = res;
+    void setCacheResolution(int res);
 
-        // set cache size
-        this->cache = std::vector<std::pair<float, float>>(res);
+    void setUseLinearInterpolation(bool inter);
 
-        // fill cache
-        float step = 1.0f / res;
-        for (int i = 0; i < res; i++) {
-            this->cache[i] = Base::operator()(i * step);
-        }
-
-    }
-
-    void setUseLinearInterpolation(bool inter) {
-        this->interpolation = inter;
-    }
-
-    virtual std::pair<float, float> operator()(float t) const {
-        float step = 1.0f / this->resulution;
-        if (this->interpolation) {
-            float pos = t / step;
-            int indexS = int(pos);
-            int indexB = indexS + 1;
-
-            double interpolation = 0.5; //pos - indexS;
-
-            return std::pair<float, float>(
-                    this->cache[indexS].first +
-                    interpolation * (this->cache[indexB].first - this->cache[indexS].first),
-                    this->cache[indexS].second +
-                    interpolation * (this->cache[indexB].second - this->cache[indexS].second)
-            );
-
-        } else {
-            int index = int(t / step + 0.5);
-            return this->cache[index];
-        }
-    };
+    virtual std::pair<float, float> operator()(float t) const;
 
 private:
-    int resulution;
     bool interpolation;
     std::vector<std::pair<float, float>> cache;
+};
+
+/** implementation **/
+
+
+template<class Base>
+void Cached<Base>::setCacheResolution(int res)
+{
+    // set cache size
+    this->cache = std::vector<std::pair<float, float>>(res);
+
+    // fill cache
+    for (int i = 0; i < res; ++i)
+    {
+        this->cache[i] = Base::operator()(float(i) / res);
+    }
+
+}
+
+template<class Base>
+void Cached<Base>::setUseLinearInterpolation(bool inter)
+{
+    this->interpolation = inter;
+}
+
+template<class Base>
+std::pair<float, float> Cached<Base>::operator()(float t) const
+{
+
+    int resolution = this->cache.size();
+
+    std::pair<float, float> result;
+
+    if (this->interpolation)
+    {
+        // calculate linear interpolation
+        int before_idx = int(t * (resolution-1));
+        int next_idx = std::ceil(t * (resolution-1));
+
+        if (before_idx == next_idx)
+        {
+            return this->cache[before_idx];
+        }
+
+        auto before_pt = this->cache[before_idx];
+        auto next_pt = this->cache[next_idx];
+
+        float x_interpol = ((t
+                             - ((next_idx / resolution)
+                                - (before_idx / resolution)))
+                            * (next_pt.first - before_pt.first))
+                           + before_pt.first;
+
+        float y_interpol = before_pt.second
+                           + ((x_interpol - before_pt.first)
+                              * (next_pt.second - before_pt.second)
+                              / (next_pt.first - before_pt.first));
+
+        result = std::pair<float, float>(x_interpol, y_interpol);
+    } else
+    {
+        // jump to rounded value
+        int index = std::lround((t * (resolution-1)));
+        result = this->cache[index];
+    }
+    return result;
 };
