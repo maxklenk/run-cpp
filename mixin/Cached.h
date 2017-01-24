3dade0b63237f8f991d6922b13cfc0e240de7ed0
Tobias Wollowski <tobias@jbbr.net>
2017-01-24 17:54:56 +0100

    fixed caching and final cleanup

--- mixin/Cached.h
+++ mixin/Cached.h
@@ -3,6 +3,11 @@
 
 #include <utility>
 #include <vector>
+#include <cmath>
+#include <iostream>
+
+
+/** interface **/
 
 template<class Base>
 class Cached : public Base
@@ -20,12 +25,12 @@ public:
     virtual std::pair<float, float> operator()(float t) const;
 
 private:
-    bool interpolation;
+    bool useLinearInterpolation = false;
     std::vector<std::pair<float, float>> cache;
 };
 
-/** implementation **/
 
+/** implementation **/
 
 template<class Base>
 void Cached<Base>::setCacheResolution(int res)
@@ -37,14 +42,15 @@ void Cached<Base>::setCacheResolution(int res)
     for (int i = 0; i < res; ++i)
     {
         this->cache[i] = Base::operator()(float(i) / res);
+//        std::cout << i << ": " << this->cache[i].first << ", " << this->cache[i].second << std::endl;
     }
 
 }
 
 template<class Base>
-void Cached<Base>::setUseLinearInterpolation(bool inter)
+void Cached<Base>::setUseLinearInterpolation(bool interpolate)
 {
-    this->interpolation = inter;
+    this->useLinearInterpolation = interpolate;
 }
 
 template<class Base>
@@ -55,36 +61,39 @@ std::pair<float, float> Cached<Base>::operator()(float t) const
 
     std::pair<float, float> result;
 
-    if (this->interpolation)
+    if (this->useLinearInterpolation)
     {
         // calculate linear interpolation
-        int before_idx = int(t * (resolution-1));
-        int next_idx = std::ceil(t * (resolution-1));
+        int before_idx = (int) (t * (resolution - 1));
+        int next_idx = (int) std::min(before_idx + 1, resolution);
 
         if (before_idx == next_idx)
         {
-            return this->cache[before_idx];
+            result = this->cache[before_idx];
         }
+        else
+        {
 
-        auto before_pt = this->cache[before_idx];
-        auto next_pt = this->cache[next_idx];
+            auto before_pt = this->cache[before_idx];
+            auto next_pt = this->cache[next_idx];
 
-        float x_interpol = ((t
-                             - ((next_idx / resolution)
-                                - (before_idx / resolution)))
-                            * (next_pt.first - before_pt.first))
-                           + before_pt.first;
+            float x_interpol = ((t
+                                 - ((next_idx / resolution)
+                                    - (before_idx / resolution)))
+                                * (next_pt.first - before_pt.first))
+                               + before_pt.first;
 
-        float y_interpol = before_pt.second
-                           + ((x_interpol - before_pt.first)
-                              * (next_pt.second - before_pt.second)
-                              / (next_pt.first - before_pt.first));
+            float y_interpol = before_pt.second
+                               + ((x_interpol - before_pt.first)
+                                  * (next_pt.second - before_pt.second)
+                                  / (next_pt.first - before_pt.first));
 
-        result = std::pair<float, float>(x_interpol, y_interpol);
+            result = std::pair<float, float>(x_interpol, y_interpol);
+        }
     } else
     {
-        // jump to rounded value
-        int index = std::lround((t * (resolution-1)));
+        // use nearest neighbour via rounding
+        int index = (int) ((t * (resolution - 1)) + 0.5);
         result = this->cache[index];
     }
     return result;
