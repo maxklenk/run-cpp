1dc73e364efd39182191b9c9af17f22be4635986
Max Klenk <max@klenk.biz>
2017-02-07 08:41:35 +0100

    fix signal solution

--- assignment7/signal/Signal.h
+++ assignment7/signal/Signal.h
@@ -6,37 +6,29 @@ template<typename... Args>
 class Signal {
 public:
 
-
     // for functions and lambdas
     template<typename Func>
-    void connect(Func&& func) {
+    void connect(Func &&func) {
         this->functions.push_back(func);
     }
 
     // for member functions applied to objects
-    template <typename T, typename R>
-    void connect(T & obj, R T::* mf)
-    {
-//        auto func = [&obj, mf](Args... args) {
-//            auto f = std::bind(mf, obj, args);
-//        };
-//
-//        this->functions.push_back(func);
+    template<typename T, typename R>
+    void connect(T &obj, R T::* mf) {
+        auto func = [=](Args... args) {
+            auto f = std::bind(mf, obj, args...);
+            f();
+        };
 
-        // TODO: store member function
-//        this->functions.push_back((obj).*mf);
-//        auto func = std::bind(mf, obj);
-//        this->functions.push_back(func);
-//        return (obj.*mf)(std::forward<Args>(args)...);
+        this->functions.push_back(func);
     }
 
-    void emit(Args&&... args) {
-        for (auto it = functions.begin(); it != functions.end(); ++it)
-        {
+    void emit(Args &&... args) {
+        for (auto it = functions.begin(); it != functions.end(); ++it) {
             (*it)(args...);
         }
     }
 
 protected:
-    std::vector<void (*)(Args...)> functions;
+    std::vector<std::function<void(Args...)>> functions;
 };
